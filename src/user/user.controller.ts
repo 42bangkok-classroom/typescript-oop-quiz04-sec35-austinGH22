e75525eacf992ebcadf86ddb9cfe9dee06978084
PhanuphongKuharungkitcharoen <Phanuphongkuharungkitcharoen@gmail.com>
2026-03-02 09:48:08 +0700

    p02

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('users') 
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('test') 
+  @Get('test')
   test(): any[] {
     return this.userService.test();
   }
