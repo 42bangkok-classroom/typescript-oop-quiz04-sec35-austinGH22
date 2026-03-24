f1a25cf600efd1c393f38bdc4a9b61004e57c753
PhanuphongKuharungkitcharoen <Phanuphongkuharungkitcharoen@gmail.com>
2026-03-24 20:14:39 +0700

    p04

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,9 @@ import { Get } from '@nestjs/common';
 import { UserService, typeInterface } from './user.service';
 import { IUser } from './user.interface';
 
+import { Param, Query } from '@nestjs/common';
+
+
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -16,4 +19,18 @@ export class UserController {
   findAll(): IUser[] {
     return this.userService.findAll();
   }
+  
+  // ใช้ :id เพื่อบอกว่าตรงนี้คือตัวแปรนะ (เช่น /users/1 ตัวเลข 1 คือ id)
+  @Get(':id')
+  findOne(
+    @Param('id') id: string,          // แกะ id มาจาก URL
+    @Query('fields') fields?: string  // แกะ fields มาจาก ?fields=...
+  ) {
+    // fields ที่ได้มาจะเป็น String ยาวๆ เช่น "firstName,lastName"
+    // เราต้องใช้คำสั่ง .split(',') เพื่อหั่นมันให้เป็น Array ก่อนส่งให้พ่อครัว
+    const fieldsArray = fields ? fields.split(',') : undefined;
+    
+    return this.userService.findOne(id, fieldsArray);
+  }
 }
+
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import { IUser } from './user.interface';
 
+import { NotFoundException } from '@nestjs/common';
+
 export interface typeInterface {
   name?: string;
   number?: number;
@@ -11,9 +13,30 @@ export class UserService {
   test(): typeInterface[] {
     return [];
   }
+
   findAll(): IUser[] {
     const fileContent = fs.readFileSync('./data/users.json', 'utf-8');
     const users = JSON.parse(fileContent) as IUser[];
     return users;
+  }
+
+  findOne(id: string, fields?: string[]): Partial<IUser>{
+    const allUsers = this.findAll()
+    // 2. ค้นหา User ที่มี id ตรงกับที่ส่งมา
+    const user = allUsers.find((u)=> u.id === id)
+    //Throw Error 
+    if(!user){
+      throw new NotFoundException('User not found')
+    }
+
+    if(fields && fields.length>0 ){
+      const filteredUser: Partial<IUser> = {}
+      fields.forEach((field) => {
+        // ดึงเฉพาะข้อมูลที่ขอ มาใส่ใน Object ใหม่ (ใช้ as keyof IUser เพื่อกัน Linter บ่น)
+        filteredUser[field as keyof IUser] = user[field as keyof IUser];
+      });
+       return filteredUser;
+    }
+    return user
   }
 }
