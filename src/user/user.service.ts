b3edbc2699952e5051eae60f6901df8556820eb1
PhanuphongKuharungkitcharoen <Phanuphongkuharungkitcharoen@gmail.com>
2026-03-24 17:52:18 +0700

    p03

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,7 +13,7 @@ export class UserService {
   }
   finAll(): IUser[] {
     const fileContent = fs.readFileSync('./data/users.json', 'utf-8');
-    const users = JSON.parse(fileContent);
+    const users = JSON.parse(fileContent) as IUser[];
     return users;
   }
 }
